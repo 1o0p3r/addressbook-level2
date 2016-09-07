a5255ec4c31bb7b6ae9520f25436ed67bc4c9395
DESKTOP-B0LRNAJ\alphanese <limweijie1994>
2016-09-07 10:23:45 +0800

    Some changes

--- src/seedu/addressbook/data/tag/Tagging.java
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -3,13 +3,31 @@ package seedu.addressbook.data.tag;
 import seedu.addressbook.data.person.Person;
 
 public class Tagging {
-	Person person;
-	Tag tag;
-	boolean isAdded;
+	private Person person;
+	private Tag tag;
+	private boolean isAdded;
+	
+	public static final String ADDPREFIX = "+";
+	public static final String DELPREFIX = "-";
 	
 	public Tagging(Person person, Tag tag, boolean isAdded) {
 		this.person = person;
 		this.tag = tag;
 		this.isAdded = isAdded;
+	}
+	public Person getPerson() {
+		return this.person;
+	}
+	public Tag getTag() {
+		return this.tag;
+	}
+	public boolean getStatus() {
+		return isAdded;
+	}
+	@Override
+	public String toString() {
+		return (getStatus()? ADDPREFIX: DELPREFIX) + getPerson().getName().toString()
+				+ getTag().toString();
+		
 	}
 }
